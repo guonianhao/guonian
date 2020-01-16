3033c98691415ed45dd20500f0429d873d20c4d8
zj <924368625@qq.com>
2020-01-16 10:19:30 +0800

    张江江上传管理员实体类

---
+++ src/main/java/com/example/mint/model/entity/Admin.java
@@ -0,0 +1,50 @@
+package com.example.mint.model.entity;
+
+public class Admin {
+
+    private Integer adminId;//管理员Id
+    private String adminName;//管理员姓名
+    private String adminPhone;//管理员手机号
+    private Integer adminType;//管理员类型
+    private Integer adminStatus;//管理员状态
+
+    public Integer getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Integer adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    public String getAdminPhone() {
+        return adminPhone;
+    }
+
+    public void setAdminPhone(String adminPhone) {
+        this.adminPhone = adminPhone;
+    }
+
+    public Integer getAdminType() {
+        return adminType;
+    }
+
+    public void setAdminType(Integer adminType) {
+        this.adminType = adminType;
+    }
+
+    public Integer getAdminStatus() {
+        return adminStatus;
+    }
+
+    public void setAdminStatus(Integer adminStatus) {
+        this.adminStatus = adminStatus;
+    }
+}
